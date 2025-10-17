c27b0187909f4681c9a9097c922bc29a4914a87b
Raz8er <n.rendic993@gmail.com>
2025-10-17 12:20:10 +0200

    Improved test data generation

--- backend/src/main/resources/db/data/V1_0_6__Add_test_data.sql
+++ backend/src/main/resources/db/data/V1_0_6__Add_test_data.sql
@@ -8,25 +8,31 @@ select 'Author' || floor(random() * 1000000)::text,
 from
     generate_series(1, 1000);
 
--- Books
 insert
 into books (title,
             isbn,
             genre,
             creation_date_time,
             publishing_date_time)
-select 'Book ' || floor(random() * 1000000)::text,
-       'ISBN-' || floor(random() * 1000000000)::text,
+select 'Book' || floor(random() * 1000000)::text,
+       LPAD(floor(random() * 1000)::text, 3, '0') || '-' ||
+       LPAD(floor(random() * 100)::text, 2, '0') || '-' ||
+       LPAD(floor(random() * 100000)::text, 5, '0') || '-' ||
+       LPAD(floor(random() * 100)::text, 2, '0') || '-' ||
+       LPAD(floor(random() * 10)::text, 1, '0'),
        (array ['Romance',
            'Thriller',
            'Comedy',
            'Fantasy',
            'History',
            'Sci-Fi'])[floor(random() * 6 + 1)],
-       now() - (random() * interval '100 years'),
-       now() - (random() * interval '100 years')
-from
-    generate_series(1, 10000);
+       sub.creation_date_time,
+       least(
+               now() - (random() * interval '365 days'),
+               sub.creation_date_time + (random() * interval '10 years')
+       )
+from (select now() - (random() * interval '100 years') as creation_date_time
+      from generate_series(1, 10000)) as sub;
 
 insert
 into books_authors (book_id,
@@ -35,4 +41,4 @@ select b.id,
        a.id
 from books b
          join authors a on
-    random() < 0.2;
+    random() < 0.05;
